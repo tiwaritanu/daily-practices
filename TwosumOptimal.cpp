274988956509be41c45f7386cfe969eeebf39a8e
Divyanshi Tiwari <145838771+tiwaritanu@users.noreply.github.com>
2025-11-25 23:16:45 +0530

    Add files via upload

---
+++ TwosumOptimal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int tar) {
+        unordered_map<int, int> m;
+        vector<int> ans;
+    
+        for (int i = 0 ; i < arr.size() ; i++) {
+        int first = arr[i];
+        int sec = tar - first;
+
+         if(m.find(sec) != m.end()) {
+            ans.push_back(i);
+            ans.push_back(m[sec]);
+            break;
+        }
+         m[first] = i;
+        } 
+         return ans;
+    }
+};
